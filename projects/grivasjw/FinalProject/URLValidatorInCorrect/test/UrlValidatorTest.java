298d940906e8b9405f02fd21ddf5207755fae1a6
William GrivasJr <grivasjw@flip1.engr.oregonstate.edu>
2019-03-11 22:48:22 -0700

    added manual test to project B

--- projects/grivasjw/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/grivasjw/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -4,7 +4,7 @@ import junit.framework.TestCase;
 
 //You can use this as a skeleton for your 3 different test approach
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
-// Again, it is up to you to use this file or not!
+//Again, it is up to you to use this file or not!
 
 
 
@@ -21,8 +21,24 @@ public class UrlValidatorTest extends TestCase {
    
    public void testManualTest()
    {
-//You can use this function to implement your manual testing	   
-	   
+	  //You can use this function to implement your manual testing
+	  UrlValidator urlValidator = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+      
+	  assertTrue(urlValidator.isValid("http://www.google.com"));
+      assertFalse(urlValidator.isValid("ftp://www.google.com"));
+	  assertFalse(urlValidator.isValid("38pt://google.com")); //38pt not valid
+	  assertTrue(urlValidator.isValid("https://www.google.com"));
+	  assertTrue(urlValidator.isValid("http://www.go.au"));
+      assertTrue(urlValidator.isValid("ftp://ftp.myexample.com"));
+      assertTrue(urlValidator.isValid("http://www.oracle.org/"));
+      assertFalse(urlValidator.isValid("1.1.1.1"));
+	  assertFalse(urlValidator.isValid("http://1.2.3.400/")); //greater than 255 not valid
+	  assertTrue(urlValidator.isValid("http://www.amazon.com/"));
+      assertFalse(urlValidator.isValid("www.amazon.com/x/y/z"));
+      assertTrue(urlValidator.isValid("http://www.amazon.com:80/abcde"));
+      assertTrue(urlValidator.isValid("http://www.amazon.com/abcde?x=y&y=z"));
+      assertTrue(urlValidator.isValid("http://www.amazon.com/abcde?x=!"));
+      
    }
    
    
