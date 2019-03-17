9a4e1144acf23ff8d9080f4fafcaed9cfca39034
William GrivasJr <grivasjw@flip3.engr.oregonstate.edu>
2019-03-17 10:27:54 -0700

    Fixed manual test

--- projects/grivasjw/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/grivasjw/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 import java.io.Serializable;
 //You can use this as a skeleton for your 3 different test approach
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
-// Again, it is up to you to use this file or not!
+//Again, it is up to you to use this file or not!
 
 
 
@@ -47,11 +47,11 @@ public class UrlValidatorTest extends TestCase {
 	  assertTrue(urlValidator.isValid("https://www.google.com"));
 	  assertTrue(urlValidator.isValid("http://www.go.au"));
 	  assertTrue(urlValidator.isValid("ftp://ftp.myexample.com"));
-	  assertTrue(urlValidator.isValid("http://www.oracle.org/"));
+	  assertFalse(urlValidator.isValid("http://www.oracle.org/.."));
 	  assertFalse(urlValidator.isValid("1.1.1.1"));
 	  assertFalse(urlValidator.isValid("http://1.2.3.400/")); //greater than 255 not valid
-	  assertTrue(urlValidator.isValid("http://www.amazon.com/"));
-	  assertFalse(urlValidator.isValid("www.amazon.com/x/y/z"));
+	  assertTrue(urlValidator.isValid("file://1.2.3.244/"));
+	  assertFalse(urlValidator.isValid("http://www.amazon.com/x/sjkd*&^%$@"));
 	  assertTrue(urlValidator.isValid("http://www.amazon.com:80/abcde"));
 	  assertTrue(urlValidator.isValid("http://www.amazon.com/abcde?x=y&y=z"));
 	  assertTrue(urlValidator.isValid("http://www.amazon.com/abcde?x=!"));	   
